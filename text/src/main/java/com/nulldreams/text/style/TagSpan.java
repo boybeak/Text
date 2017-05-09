4f65765db4f715db43e2812b809d43ce9f0d5715
boybeak <boybeak@qq.com>
2017-05-10 00:24:24 +0800

    add some code

--- text/src/main/java/com/nulldreams/text/style/TagSpan.java
+++ text/src/main/java/com/nulldreams/text/style/TagSpan.java
@@ -1,22 +1,28 @@
 package com.nulldreams.text.style;
 
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.support.annotation.IntRange;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.text.TextPaint;
 import android.text.style.ClickableSpan;
+import android.text.style.ReplacementSpan;
 import android.view.View;
 
 /**
  * Created by boybe on 2017/5/9.
  */
 
-public class TagSpan extends ClickableSpan {
+public class TagSpan extends ReplacementSpan {
 
     @Override
-    public void onClick(View widget) {
-
+    public int getSize(@NonNull Paint paint, CharSequence text, @IntRange(from = 0) int start, @IntRange(from = 0) int end, @Nullable Paint.FontMetricsInt fm) {
+        return 0;
     }
 
     @Override
-    public void updateDrawState(TextPaint ds) {
-        super.updateDrawState(ds);
+    public void draw(@NonNull Canvas canvas, CharSequence text, @IntRange(from = 0) int start, @IntRange(from = 0) int end, float x, int top, int y, int bottom, @NonNull Paint paint) {
+
     }
 }
